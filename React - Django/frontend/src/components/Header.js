00440c9fb7d3fb300e83497b25b8661b5a1e3ddf
webapp-master <ogundirantoluwani@gmail.com>
2023-10-22 18:50:46 +0100

    before- learning, trial and error begins

--- React - Django/frontend/src/components/Header.js
+++ React - Django/frontend/src/components/Header.js
@@ -1,79 +1,62 @@
 import React from "react";
-import {Navbar,Nav,Container,Row,NavDropdown} from 'react-bootstrap'
-import { LinkContainer } from 'react-router-bootstrap'
+import { Navbar, Nav, Container, Row, NavDropdown } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 import { logout } from "../actions/userActions";
-import { useDispatch,useSelector } from 'react-redux'
-
+import { useDispatch, useSelector } from "react-redux";
 
 function Header() {
-
-  const userLogin = useSelector(state=>state.userLogin)
-  const {userInfo} = userLogin
-  const dispatch = useDispatch()
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+  const dispatch = useDispatch();
   const logoutHandler = () => {
-    dispatch(logout())
-  }
-
-
-
-
+    dispatch(logout());
+  };
 
   return (
     <div>
       <Navbar bg="dark" variant="dark">
-          <Container>
+        <Container>
+          <LinkContainer to="/" LinkContainer>
+            <Navbar.Brand>Shopnet</Navbar.Brand>
+          </LinkContainer>
 
-          <LinkContainer to="/"LinkContainer>
-        <Navbar.Brand>A.R.K</Navbar.Brand>
-        </LinkContainer>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="mr-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <LinkContainer to="/">
+                <Nav.Link>
+                  <i className="fas fa-home"></i> Home
+                </Nav.Link>
+              </LinkContainer>
 
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="mr-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          >
-            <LinkContainer to="/">
-            <Nav.Link><i className="fas fa-home"></i> Home</Nav.Link>
-            </LinkContainer>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart"></i> Cart
+                </Nav.Link>
+              </LinkContainer>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id="username">
+                  <LinkContainer to="/profile">
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
 
-            <LinkContainer to="/cart">
-            <Nav.Link><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
-            </LinkContainer>
-{userInfo?(
-  <NavDropdown title={userInfo.name} id='username'>
-                                    <LinkContainer to='/profile'>
-                                        <NavDropdown.Item>Profile</NavDropdown.Item>
-                                    </LinkContainer>
-
-                                    <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
-
-                                </NavDropdown>
-
-):
-
-(
-
-
-  <LinkContainer to="/login">
-  <Nav.Link><i className="fas fa-user"></i> Login</Nav.Link>
-  </LinkContainer>
-)
-
-}
-
-
-
-
-
-
-
-            
-         
-          </Nav>
-          
-        </Navbar.Collapse>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to="/login">
+                  <Nav.Link>
+                    <i className="fas fa-user"></i> Login
+                  </Nav.Link>
+                </LinkContainer>
+              )}
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </div>
