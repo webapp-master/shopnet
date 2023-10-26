94e393c78bb2eaeaae6a736f346f10aad497e44d
webapp-master <ogundirantoluwani@gmail.com>
2023-10-26 23:52:16 +0100

    I want to resolve login and register

--- React - Django/backend/app/models.py
+++ React - Django/backend/app/models.py
@@ -1,47 +1,43 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 from decimal import Decimal
 from django.utils import timezone
 from django.conf import settings
 
-
 class Product(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
-    name=models.CharField(max_length=200,null=True,blank=True)
-    image=models.ImageField(null=True,blank=True)
-    brand=models.CharField(max_length=200,null=True,blank=True)
-    category=models.CharField(max_length=200,null=True,blank=True)
-    description=models.TextField(null=True,blank=True)
-    rating=models.DecimalField(max_digits=7,decimal_places=2,null=True,blank=True)
-    numReviews=models.IntegerField(null=True,blank=True,default=0)
-    price=models.DecimalField(max_digits=7,decimal_places=2,null=True,blank=True)
-    countInStock=models.IntegerField(null=True,blank=True,default=0)
-    createdAt=models.DateTimeField(auto_now_add=True)
-    _id=models.AutoField(primary_key=True,editable=False)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    brand = models.CharField(max_length=200, null=True, blank=True)
+    category = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    rating = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    numReviews = models.IntegerField(null=True, blank=True, default=0)
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    countInStock = models.IntegerField(null=True, blank=True, default=0)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    _id = models.AutoField(primary_key=True, editable=False)
 
     def __str__(self):
         return self.name
 
 class Review(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
-    product=models.ForeignKey(Product,on_delete=models.SET_NULL,null=True)
-    name=models.CharField(max_length=200,null=True,blank=True)
-    rating=models.IntegerField(null=True,blank=True,default=0)
-    comment=models.TextField(null=True,blank=True)
-    _id=models.AutoField(primary_key=True,editable=False)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    rating = models.IntegerField(null=True, blank=True, default=0)
+    comment = models.TextField(null=True, blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
 
     def __str__(self):
         return str(self.rating)
-
-
-
 
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
     payment_amount = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     payment_status = models.BooleanField(default=False)
-    paid_at = models.DateTimeField(auto_now_add=False, null=True, blank=True)
-    _id=models.AutoField(primary_key=True,editable=False)
+    paid_at = models.DateTimeField(null=True, blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
 
     def process_order(self):
         if not self.payment_status:
@@ -56,39 +52,29 @@ class Order(models.Model):
                 return True
         return False
 
-
-
-
-
 class OrderItem(models.Model):
-    product=models.ForeignKey(Product,on_delete=models.SET_NULL,null=True)
-    order=models.ForeignKey(Order,on_delete=models.SET_NULL,null=True)
-    name=models.CharField(max_length=200,null=True,blank=True)
-    qty=models.IntegerField(null=True,blank=True,default=0)
-    price=models.DecimalField(max_digits=7,decimal_places=2,null=True,blank=True)
-    image=models.CharField(max_length=200,null=True,blank=True)
-    _id=models.AutoField(primary_key=True,editable=False)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    qty = models.IntegerField(null=True, blank=True, default=0)
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    image = models.CharField(max_length=200, null=True, blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
 
-        
     def __str__(self):
         return self.name
 
 class ShippingAddress(models.Model):
-    order=models.OneToOneField(Order,on_delete=models.CASCADE,null=True,blank=True)
-    address=models.CharField(max_length=200,null=True,blank=True)
-    city=models.CharField(max_length=200,null=True,blank=True)
-    postalCode=models.CharField(max_length=200,null=True,blank=True)
-    country=models.CharField(max_length=200,null=True,blank=True)
-    shippingPrice=models.DecimalField(max_digits=7,decimal_places=2,null=True,blank=True)
-    _id=models.AutoField(primary_key=True,editable=False)
+    order = models.OneToOneField(Order, on_delete=models.CASCADE, null=True, blank=True)
+    address = models.CharField(max_length=200, null=True, blank=True)
+    city = models.CharField(max_length=200, null=True, blank=True)
+    postalCode = models.CharField(max_length=200, null=True, blank=True)
+    country = models.CharField(max_length=200, null=True, blank=True)
+    shippingPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
 
     def __str__(self):
         return self.address
-    
-
-
-
-from django.contrib.auth.models import AbstractUser
 
 class UserProfile(AbstractUser):
     # Your custom fields go here
