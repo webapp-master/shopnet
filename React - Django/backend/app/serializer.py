0c32a26b275039ef458ae8320198aa83aca60938
webapp-master <ogundirantoluwani@gmail.com>
2023-11-14 20:58:49 +0100

    making progress in resolving issue: No 'Cart' for user

--- React - Django/backend/app/serializer.py
+++ React - Django/backend/app/serializer.py
@@ -9,38 +9,12 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = '__all__'
 
-class UserSerializer(serializers.ModelSerializer):
-    name = serializers.SerializerMethodField(read_only=True)
-    isAdmin = serializers.SerializerMethodField(read_only=True)
-
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'name', 'isAdmin']
-
-    def get_name(self, obj):
-        name = obj.first_name
-        if name == "":
-            name = obj.email
-        return name
-
-    def get_isAdmin(self, obj):
-        return obj.is_staff
-
-class UserSerializerWithToken(UserSerializer):
-    token = serializers.SerializerMethodField(read_only=True)
-
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'name', 'isAdmin', 'token']
-
-    def get_token(self, obj):
-        token = RefreshToken.for_user(obj)
-        return str(token.access_token)
 
 class CartItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = CartItem
         fields = '__all__'
+
 
 class CartSerializer(serializers.ModelSerializer):
     cart_items = CartItemSerializer(many=True, read_only=True)
@@ -48,6 +22,44 @@ class CartSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cart
         fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField(read_only=True)
+    isAdmin = serializers.SerializerMethodField(read_only=True)
+    cart = serializers.SerializerMethodField(read_only=True)  # Include cart here
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'name', 'isAdmin', 'cart']  # Add cart to the fields list
+
+    def get_name(self, obj):
+        name = obj.first_name
+        if not name:
+            name = obj.email
+        return name
+
+    def get_isAdmin(self, obj):
+        return obj.is_staff
+
+    def get_cart(self, obj):
+        # Fetch and serialize the user's cart information here
+        cart = Cart.objects.get_or_create(user=obj)  # Assuming Cart is related to User
+        serialized_cart = CartSerializer(cart).data
+        return serialized_cart
+
+
+class UserSerializerWithToken(UserSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+    cart = CartSerializer(read_only=True)  # Assuming there's a relationship between User and Cart
+
+    class Meta(UserSerializer.Meta):
+        fields = ['id', 'username', 'email', 'name', 'isAdmin', 'token', 'cart']
+
+    def get_token(self, obj):
+        token = RefreshToken.for_user(obj)
+        return str(token.access_token)
+
 
 
 
