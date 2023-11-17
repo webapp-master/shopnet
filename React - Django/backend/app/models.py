b053e2c8f8161d3c84c94e2fe77fb49a5577d4b5
webapp-master <ogundirantoluwani@gmail.com>
2023-11-17 14:56:33 +0100

    making further moves

--- React - Django/backend/app/models.py
+++ React - Django/backend/app/models.py
@@ -3,10 +3,6 @@ from django.contrib.auth.models import User
 
 
 
-
-
-
-
 class Product(models.Model):
     user=models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     name=models.CharField(max_length=200,null=True,blank=True)
