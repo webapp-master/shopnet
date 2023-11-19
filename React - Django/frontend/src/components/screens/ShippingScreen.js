59f15b3585ec90e1a112af6272aff9c56e471f41
webapp-master <ogundirantoluwani@gmail.com>
2023-11-19 16:35:34 +0100

    ShippingScreen looking better... but needs more work

--- React - Django/frontend/src/components/screens/ShippingScreen.js
+++ React - Django/frontend/src/components/screens/ShippingScreen.js
@@ -15,11 +15,42 @@ const ShippingScreen = ({ history }) => {
     0
   );
 
-  // ... (rest of your component)
+
+
+
+  const [state, setState] = useState("");
+  const [city, setCity] = useState("");
+  const [area, setArea] = useState("");
+  const [street, setStreet] = useState("");
+  const [houseNumber, setHouseNumber] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(
+      saveShippingAddress({
+        state,
+        city,
+        area,
+        street,
+        houseNumber,
+        phoneNumber,
+      })
+    );
+    history.push("/login?redirect=buy");
+  };
+
+  
+  
+
+
+
 
   return (
     <Container>
+
       <Row>
+
         <Col md={6}>
           <Card>
             <Card.Body>
@@ -40,8 +71,100 @@ const ShippingScreen = ({ history }) => {
             </Card.Body>
           </Card>
         </Col>
+
         <Col md={6}>
-          {/* Rest of your form for shipping address */}
+
+
+        <div>
+            <h1>Shipping Address</h1>
+            <Form onSubmit={submitHandler}>
+
+
+              {/* Shipping Address form */}
+
+              <Row className="mb-3">
+                <Form.Group as={Col} controlId="state">
+                  <Form.Label>State</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={state}
+                    onChange={(e) => setState(e.target.value)}
+                    required
+                  />
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="city">
+                  <Form.Label>City</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={city}
+                    onChange={(e) => setCity(e.target.value)}
+                    required
+                  />
+                </Form.Group>
+              </Row>
+
+              <Row className="mb-3">
+                <Form.Group as={Col} controlId="area">
+                  <Form.Label>Area</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={area}
+                    onChange={(e) => setArea(e.target.value)}
+                    required
+                  />
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="street">
+                  <Form.Label>Street</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={street}
+                    onChange={(e) => setStreet(e.target.value)}
+                    required
+                  />
+                </Form.Group>
+              </Row>
+
+              <Row className="mb-3">
+                <Form.Group as={Col} controlId="houseNumber">
+                  <Form.Label>House Number</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={houseNumber}
+                    onChange={(e) => setHouseNumber(e.target.value)}
+                    required
+                  />
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="phoneNumber">
+                  <Form.Label>Phone Number</Form.Label>
+                  <Form.Control
+                    type="number"
+                    value={phoneNumber}
+                    onChange={(e) => setPhoneNumber(e.target.value)}
+                    required
+                  />
+                </Form.Group>
+              </Row>
+
+
+
+              {/* Submit button */}
+
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+
+            <div>
+              {/* Shipping Cost */}
+              <h2>Shipping Cost</h2>
+              <p>Shipping Cost: $1,000</p>
+            </div>
+          </div>
+
+
         </Col>
       </Row>
     </Container>
