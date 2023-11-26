94fc22242b1eae2d8aca07554e7d3f0e781d8bd4
webapp-master <ogundirantoluwani@gmail.com>
2023-11-27 00:13:57 +0100

    Product.js: dev (very nice)

--- React - Django/frontend/src/components/Product.js
+++ React - Django/frontend/src/components/Product.js
@@ -1,39 +1,45 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import Rating from "../components/Rating";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
 function Product({ product }) {
+
   return (
-    <Card classname="my-3 p-3 rounded">
+
+    <Card className="my-3 p-3 rounded">
+
       <Link to={`/product/${product.id}`}>
         <Card.Img src={product.image} />
       </Link>
 
-      <Card.Body>
-        <Link to={`/product/${product.id}`}>
+      <Card.Body className="d-flex flex-column align-items-center justify-content-center">
+
+        <Link to={`/product/${product.id}`} className="text-center">
           <Card.Title as="div">
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
 
-        <Card.Text as="div">
-            <div className="my-3">
-                {product.rating} from {product.numReviews} reviews
-            </div>
+        <Card.Text as="div" className="text-center">
+          <div className="my-3">
+            {product.rating} rating from {product.numReviews} reviews
+          </div>
         </Card.Text>
-        
-        <Card.Text as="h3">
-            ${product.price}
+
+        <Card.Text as="h3" className="text-center">
+          ${product.price}
         </Card.Text>
 
         <Rating
-                value={product.rating}
-                text={`${product.numReviews} reviews`}
-                color={"#f8e825"}
-              />
+          value={product.rating}
+          color={"#f8e825"}
+        />
+
       </Card.Body>
+
     </Card>
+
   );
 }
 
