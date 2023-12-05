69c34ab66c2a7fb2199a056f7b90a31e348eb994
webapp-master <ogundirantoluwani@gmail.com>
2023-12-05 19:05:10 +0100

    trying to divide the card body into two

--- React - Django/frontend/src/components/screens/BuyScreen.js
+++ React - Django/frontend/src/components/screens/BuyScreen.js
@@ -1,40 +1,53 @@
-import React from "react";
-import { Button, Card, Container } from "react-bootstrap";
+import React from 'react';
+import { Button, Card, Container } from 'react-bootstrap';
 
 const BuyScreen = () => {
-  const header_footer_color = "#bdbdbd"; // Deeper version of the color
+  const headerFooterColor = "#bdbdbd"; // Deeper version of the color
+
+  const cardBodyGrid = {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(2, 1fr)', // Divide the body into two columns
+    gap: '10px', // Gap between grid items
+  };
+
   return (
     <Container className="d-flex justify-content-center align-items-center">
-      <Card style={{ width: "60rem" }}>
-        <Card.Header className="text-center" style={{ backgroundColor: header_footer_color }}>
+      <Card style={{ width: '60rem' }}>
+        <Card.Header className="text-center" style={{ backgroundColor: headerFooterColor }}>
           <h4>Place your Order</h4>
         </Card.Header>
-        <Card.Body className="text-dark">
+        <Card.Body style={cardBodyGrid}>
           <Card.Text>
-            <p>Wallet Balance: $700</p>
+            <p>Wallet Balance:</p>
             <p>Product List:</p>
+            <p>No. of Items:</p>
+            <p>Cost of Products in Your Cart:</p>
+            <p>Shipping Cost:</p>
+            <p>Tax:</p>
+            <p>Total Amount:</p>
+            <p>Shipping Address:</p>
+            <p>Phone Number:</p>
+          </Card.Text>
+          <Card.Text>
+            <p>$700</p>
             <ul>
               <li>AIRPODS WIRELESS BLUETOOTH HEADPHONES</li>
               <li>CANNON EOS 80D DSLR CAMERA</li>
               <li>IPHONE 11 PRO 256GB MEMORY</li>
             </ul>
-            <p>No. of Items: 3</p>
-            <p>Cost of Products in Your Cart: $523</p>
-            <p>Shipping Cost: $7</p>
-            <p>Tax: $1</p>
-            <p>Total Amount: $531</p>
-            <p>
-              Shipping Address: 04, grace-allegro street, Agodongbo, Oyo, Oyo
-              State
-            </p>
-            <p>Phone Number: 08034342186</p>
+            <p>3</p>
+            <p>$523</p>
+            <p>$7</p>
+            <p>$1</p>
+            <p>$531</p>
+            <p>04, grace-allegro street, Agodongbo, Oyo, Oyo State</p>
+            <p>08034342186</p>
           </Card.Text>
         </Card.Body>
-        <Card.Footer className="d-flex justify-content-between align-items-center" style={{ backgroundColor: header_footer_color }}>
+        <Card.Footer className="d-flex justify-content-between align-items-center" style={{ backgroundColor: headerFooterColor }}>
           <Button variant="secondary">Cart</Button>
           <Button variant="primary">Purchase</Button>
           <Button variant="success">Shipping</Button>
-          
         </Card.Footer>
       </Card>
     </Container>
@@ -42,5 +55,3 @@ const BuyScreen = () => {
 };
 
 export default BuyScreen;
-
-
