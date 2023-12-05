813feb753a2a9d3f79d652f09728c60e2abf77a9
webapp-master <ogundirantoluwani@gmail.com>
2023-12-05 09:04:29 +0100

    keep this code for BuyScreen.js: it has logic that relies on the redux store

--- React - Django/frontend/src/components/screens/BuyScreen.js
+++ React - Django/frontend/src/components/screens/BuyScreen.js
@@ -1,25 +1,68 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Button, Container, Row, Col, Card } from 'react-bootstrap';
 
 const BuyScreen = () => {
-  const [successMessage, setSuccessMessage] = useState('');
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
 
-  const handleBuyClick = () => {
-    // Perform the order placement logic here
+  const totalItems = cartItems.reduce((acc, item) => acc + item.qty, 0);
+  const totalPrice = cartItems.reduce((acc, item) => acc + item.qty * item.price, 0);
 
-    // Display a success message
-    setSuccessMessage('Order has been placed successfully');
-  };
+  // Fetch the wallet balance - assuming it's in Redux state
+  const walletBalance = useSelector((state) => state.user.walletBalance);
 
   return (
-    <div>
-      <h1>Order Summary</h1>
-      <div>
-        <h2>Total Amount to be Paid: $100.00</h2>
-        <p>Wallet: $50.00</p>
-      </div>
-      <button onClick={handleBuyClick}>BUY</button>
-      {successMessage && <div>{successMessage}</div>}
-    </div>
+    <Container>
+      <h2 className="mt-4">Buy Screen</h2>
+      <Row className="mt-4">
+        <Col md={6}>
+          <Card>
+            <Card.Body>
+              <Card.Title>Wallet Balance</Card.Title>
+              <Card.Text>{`$${walletBalance}`}</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Row className="mt-4">
+        <Col md={6}>
+          <Card>
+            <Card.Body>
+              <Card.Title>Product List</Card.Title>
+              {cartItems.map((item) => (
+                <div key={item.product}>
+                  <p>{item.name}</p>
+                </div>
+              ))}
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Row className="mt-4">
+        <Col md={6}>
+          <Card>
+            <Card.Body>
+              <Card.Title>No. of Items</Card.Title>
+              <Card.Text>{totalItems}</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col md={6}>
+          <Card>
+            <Card.Body>
+              <Card.Title>Total Amount</Card.Title>
+              <Card.Text>{`$${totalPrice.toFixed(2)}`}</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Row className="mt-4">
+        <Col>
+          <Button variant="primary">Buy</Button>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
