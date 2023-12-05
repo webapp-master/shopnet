b68318f160ea6824e0df0daf637812c5b5f896f9
webapp-master <ogundirantoluwani@gmail.com>
2023-12-05 18:16:28 +0100

    getting somewhere with my frontend design for BuyScreen

--- React - Django/frontend/src/components/screens/BuyScreen.js
+++ React - Django/frontend/src/components/screens/BuyScreen.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import { Button, Card, Container } from 'react-bootstrap';
+import React from "react";
+import { Button, Card, Container } from "react-bootstrap";
 
 const BuyScreen = () => {
+  const header_footer_color = "#bdbdbd"; // Deeper version of the color
   return (
-    <Container className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
-      <Card style={{ width: '30rem' }}>
-        <Card.Header className="text-center">
+    <Container className="d-flex justify-content-center align-items-center">
+      <Card style={{ width: "60rem" }}>
+        <Card.Header className="text-center" style={{ backgroundColor: header_footer_color }}>
           <h4>Place your Order</h4>
         </Card.Header>
-        <Card.Body>
+        <Card.Body className="text-dark">
           <Card.Text>
             <p>Wallet Balance: $700</p>
             <p>Product List:</p>
@@ -22,10 +23,18 @@ const BuyScreen = () => {
             <p>Shipping Cost: $7</p>
             <p>Tax: $1</p>
             <p>Total Amount: $531</p>
+            <p>
+              Shipping Address: 04, grace-allegro street, Agodongbo, Oyo, Oyo
+              State
+            </p>
+            <p>Phone Number: 08034342186</p>
           </Card.Text>
         </Card.Body>
-        <Card.Footer className="text-center">
-          <Button variant="primary">Buy</Button>
+        <Card.Footer className="d-flex justify-content-between align-items-center" style={{ backgroundColor: header_footer_color }}>
+          <Button variant="secondary">Cart</Button>
+          <Button variant="primary">Purchase</Button>
+          <Button variant="success">Shipping</Button>
+          
         </Card.Footer>
       </Card>
     </Container>
@@ -33,3 +42,5 @@ const BuyScreen = () => {
 };
 
 export default BuyScreen;
+
+
