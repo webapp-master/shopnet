f5677f1a9a36b79f6fd8aa543cfa8cce2b39c0fa
webapp-master <ogundirantoluwani@gmail.com>
2023-12-06 11:18:20 +0100

    the card is now looking fantastic

--- React - Django/frontend/src/components/screens/BuyScreen.js
+++ React - Django/frontend/src/components/screens/BuyScreen.js
@@ -1,19 +1,23 @@
-import React from "react";
-import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import React from 'react';
+import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 
 const BuyScreen = () => {
   const headerFooterColor = "#bdbdbd"; // Deeper version of the color
   const cardBodyColor = "#fff7eb"; // Desired background color for the card body
   const listBorderStyle = {
-    listStyleType: "none",
-    padding: "10px",
-    border: "1px solid #000",
-    borderRadius: "75px",
-    backgroundColor: "#fff",
+    listStyleType: 'none',
+    padding: '10px',
+    border: '1px solid #000',
+    borderRadius: '25px',
+    backgroundColor: '#fff',
   };
   const buttonStyle = {
-    width: "100%", // Setting a fixed width for all buttons
-    margin: "5px", // Adding some space between buttons
+    width: '100%', // Setting a fixed width for all buttons
+    margin: '5px', // Adding some space between buttons
+  };
+
+  const boldText = {
+    fontWeight: '900', // Making the text bold
   };
 
   return (
@@ -26,27 +30,18 @@ const BuyScreen = () => {
           <img src="product_image_url_2" alt="Product 2" />
           <img src="product_image_url_3" alt="Product 3" />
         </Col>
-
         {/* Right Column: Order Details */}
+
         <Col xs={12} md={6}>
-          <Card style={{ borderRadius: "25px" }}>
-            {/* Adjusted border-radius */}
-            <Card.Header
-              className="text-center"
-              style={{
-                backgroundColor: headerFooterColor,
-                borderRadius: "25px",
-              }}
-            >
+        <Card style={{ border: 'none' }}> {/* Set border to none */} 
+
+            <Card.Header className="text-center" style={{ backgroundColor: headerFooterColor, borderRadius: "25px"}}>
               <h4>Place your Order</h4>
             </Card.Header>
-            <Card.Body
-              className="text-dark"
-              style={{ backgroundColor: cardBodyColor, borderRadius: "25px" }}
-            >
-              {/* Adjusted border-radius */}
-              <Card.Text className="text-center">
-                <p>Wallet Balance: $700</p>
+
+            <Card.Body className="text-dark" style={{ backgroundColor: cardBodyColor, borderRadius: "75px" }}>
+            <Card.Text className="text-center">
+                <p style={boldText}>Wallet Balance: $700</p>
                 <p>Product List:</p>
                 <ul style={listBorderStyle}>
                   <li>AIRPODS WIRELESS BLUETOOTH HEADPHONES</li>
@@ -57,44 +52,18 @@ const BuyScreen = () => {
                 <p>Cost of Products in Your Cart: $523</p>
                 <p>Shipping Cost: $7</p>
                 <p>Tax: $1</p>
-                <p>Total Amount: $531</p>
+                <p style={boldText}>Total Amount: $531</p>
                 <p>
-                  Shipping Address: 04, grace-allegro street, Agodongbo, Oyo,
-                  Oyo State
+                  Shipping Address: 04, grace-allegro street, Agodongbo, Oyo, Oyo
+                  State
                 </p>
                 <p>Phone Number: 08034342186</p>
               </Card.Text>
             </Card.Body>
-
-            <Card.Footer
-              className="d-flex justify-content-between align-items-center"
-              style={{
-                backgroundColor: headerFooterColor,
-                borderRadius: "25px",
-              }}
-            >
-              {/* Adjusted border-radius */}
-              <Button
-                variant="secondary"
-                className="rounded-pill"
-                style={buttonStyle}
-              >
-                Cart
-              </Button>
-              <Button
-                variant="primary"
-                className="rounded-pill"
-                style={buttonStyle}
-              >
-                Purchase
-              </Button>
-              <Button
-                variant="success"
-                className="rounded-pill"
-                style={buttonStyle}
-              >
-                Shipping
-              </Button>
+            <Card.Footer className="d-flex justify-content-between align-items-center" style={{ backgroundColor: headerFooterColor, borderRadius: "25px"}}>
+              <Button variant="secondary" className="rounded-pill" style={buttonStyle}>Cart</Button>
+              <Button variant="primary" className="rounded-pill" style={buttonStyle}>Purchase</Button>
+              <Button variant="success" className="rounded-pill" style={buttonStyle}>Shipping</Button>
             </Card.Footer>
           </Card>
         </Col>
