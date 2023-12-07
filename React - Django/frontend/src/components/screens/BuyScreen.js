7ef70412b27c173442fe73b702053bfa21f304ca
webapp-master <ogundirantoluwani@gmail.com>
2023-12-07 22:24:02 +0100

    adjusting the images layout

--- React - Django/frontend/src/components/screens/BuyScreen.js
+++ React - Django/frontend/src/components/screens/BuyScreen.js
@@ -2,12 +2,9 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Col, Container, Row, Card, Button } from 'react-bootstrap';
 
-
-
 const BuyScreen = () => {
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
-
 
   const headerFooterColor = "#bdbdbd"; // Deeper version of the color
   const cardBodyColor = "#fff7eb"; // Desired background color for the card body
@@ -27,62 +24,90 @@ const BuyScreen = () => {
     fontWeight: '900', // Making the text bold
   };
 
+  const renderProductImages = () => {
+    const imageRows = [];
+    const isOdd = cartItems.length % 2 !== 0;
+    for (let i = 0; i < cartItems.length; i += 2) {
+      const item1 = cartItems[i];
+      const item2 = cartItems[i + 1];
+      imageRows.push(
+        <Row key={i} className="justify-content-center mb-3">
+          <Col xs={12} md={6} className="text-center">
+            <img
+              src={item1.image}
+              alt={item1.name}
+              style={{ maxWidth: '100%', marginBottom: '10px', borderRadius: '8px' }}
+            />
+          </Col>
+          <Col xs={12} md={6} className="text-center">
+            {item2 && (
+              <img
+                src={item2.image}
+                alt={item2.name}
+                style={{ maxWidth: '100%', marginBottom: '10px', borderRadius: '8px' }}
+              />
+            )}
+            {!item2 && isOdd && <div style={{ width: '50%', marginBottom: '10px' }} />}
+            {/* Empty placeholder Col for alignment */}
+          </Col>
+        </Row>
+      );
+    }
+    return imageRows;
+  };
+
   return (
     <Container>
       <Row className="justify-content-center align-items-center">
 
         {/* Left Column: Product Images */}
-        <Col xs={12} md={6} className="text-center">
-          {/* Display product images from cartItems */}
-          {cartItems.map((item) => (
-            <img
-              key={item.product}
-              src={item.image} // Assuming 'image' is the property in your cartItem object containing the image URL
-              alt={item.name} // Assuming 'name' is the property in your cartItem object containing the product name
-              style={{ maxWidth: '100%', marginBottom: '10px', borderRadius: '8px' }}
-            />
-          ))}
+        <Col xs={12} md={6}>
+          {renderProductImages()}
         </Col>
 
 
-
         {/* Right Column: Order Details */}
-
         <Col xs={12} md={6}>
-        <Card style={{ border: 'none' }}> {/* Set border to none */} 
+          <Card style={{ border: 'none' }}> {/* Set border to none */} 
+
 
             <Card.Header className="text-center" style={{ backgroundColor: headerFooterColor, borderRadius: "25px"}}>
               <h4>Place your Order</h4>
             </Card.Header>
 
             <Card.Body className="text-dark" style={{ backgroundColor: cardBodyColor, borderRadius: "75px" }}>
-            <Card.Text className="text-center">
-                <p style={boldText}>Wallet Balance: $700</p>
-                <p>Product List:</p>
-                <ul style={listBorderStyle}>
-                  <li>AIRPODS WIRELESS BLUETOOTH HEADPHONES</li>
-                  <li>CANNON EOS 80D DSLR CAMERA</li>
-                  <li>IPHONE 11 PRO 256GB MEMORY</li>
-                </ul>
-                <p>No. of Items: 3</p>
-                <p>Cost of Products in Your Cart: $523</p>
-                <p>Shipping Cost: $7</p>
-                <p>Tax: $1</p>
-                <p style={boldText}>Total Amount: $531</p>
-                <p>
-                  Shipping Address: 04, grace-allegro street, Agodongbo, Oyo, Oyo
-                  State
-                </p>
-                <p>Phone Number: 08034342186</p>
+
+              <Card.Text className="text-center">
+                  <p style={boldText}>Wallet Balance: $700</p>
+                  <p>Product List:</p>
+                  <ul style={listBorderStyle}>
+                    <li>AIRPODS WIRELESS BLUETOOTH HEADPHONES</li>
+                    <li>CANNON EOS 80D DSLR CAMERA</li>
+                    <li>IPHONE 11 PRO 256GB MEMORY</li>
+                  </ul>
+                  <p>No. of Items: 3</p>
+                  <p>Cost of Products in Your Cart: $523</p>
+                  <p>Shipping Cost: $7</p>
+                  <p>Tax: $1</p>
+                  <p style={boldText}>Total Amount: $531</p>
+                  <p>
+                    Shipping Address: 04, grace-allegro street, Agodongbo, Oyo, Oyo
+                    State
+                  </p>
+                  <p>Phone Number: 08034342186</p>
               </Card.Text>
             </Card.Body>
+
             <Card.Footer className="d-flex justify-content-between align-items-center" style={{ backgroundColor: headerFooterColor, borderRadius: "25px"}}>
               <Button variant="secondary" className="rounded-pill" style={buttonStyle}>Cart</Button>
               <Button variant="primary" className="rounded-pill" style={buttonStyle}>Purchase</Button>
               <Button variant="success" className="rounded-pill" style={buttonStyle}>Shipping</Button>
             </Card.Footer>
+
           </Card>
         </Col>
+
+
       </Row>
     </Container>
   );
