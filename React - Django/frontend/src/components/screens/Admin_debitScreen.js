548faff76090e8088d899884af54b0c10c1dd835
webapp-master <ogundirantoluwani@gmail.com>
2023-12-28 11:28:19 +0100

    getting better

--- React - Django/frontend/src/components/screens/Admin_debitScreen.js
+++ React - Django/frontend/src/components/screens/Admin_debitScreen.js
@@ -36,89 +36,92 @@ const Admin_debitScreen = () => {
   };
 
   return (
-    <div className="container" style={{ marginTop: "70px", border: "2px solid #ccc", borderRadius: "10px", padding: "20px" }}>
+    <div
+      className="container"
+      style={{
+        marginTop: "70px",
+        border: "2px solid #ccc",
+        borderRadius: "10px",
+        padding: "20px",
+      }}
+    >
+      <div className="container">
 
+        <h2
+          style={{
+            color: "red",
+            textAlign: "center",
+            marginBottom: "2rem",
+            textShadow: "3px 3px 4px rgba(0, 0, 0, 0.5)",
+          }}
+        >
+          Debit Customer's Wallet
+        </h2>
+        
 
-      <h2
-        style={{
-          color: "red",
-          textAlign: "center",
-          marginBottom: "2rem",
-          textShadow: "3px 3px 4px rgba(0, 0, 0, 0.5)",
-        }}
-      >
-        Debit Customer's Wallet
-      </h2>
+        <div className="row">
+          <div className="col-md-6 offset-md-3">
+            <div className="card">
+              <div className="card-body">
+                {error && <div className="alert alert-danger">{error}</div>}
+                {success && (
+                  <div className="alert alert-success">{success}</div>
+                )}
 
-      <div className="row">
-        <div className="col-md-6 offset-md-3">
+                <form>
+                  <div className="form-group debit-remove-shadow">
+                    <label htmlFor="username">Username:</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="username"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                      style={{
+                        borderRadius: "3rem",
+                        boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+                      }}
+                    />
+                  </div>
 
-        <div className="card">
+                  <div className="form-group debit-remove-shadow">
+                    <label htmlFor="amount">Amount:</label>
+                    <input
+                      type="number"
+                      className="form-control"
+                      id="amount"
+                      value={amount}
+                      onChange={(e) => setAmount(e.target.value)}
+                      style={{
+                        borderRadius: "3rem",
+                        boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)", // Adjust shadow values as needed
+                      }}
+                    />
+                  </div>
 
-
-            <div className="card-body">
-              {error && <div className="alert alert-danger">{error}</div>}
-              {success && <div className="alert alert-success">{success}</div>}
-
-              <form>
-                <div className="form-group debit-remove-shadow">
-                  <label htmlFor="username">Username:</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    style={{
-                      borderRadius: "3rem",
-                      boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
-                    }}
-                  />
-                </div>
-
-                <div className="form-group debit-remove-shadow">
-                  <label htmlFor="amount">Amount:</label>
-                  <input
-                    type="number"
-                    className="form-control"
-                    id="amount"
-                    value={amount}
-                    onChange={(e) => setAmount(e.target.value)}
-                    style={{
-                      borderRadius: "3rem",
-                      boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)", // Adjust shadow values as needed
-                    }}
-                  />
-                </div>
-
-                <div className="d-flex justify-content-center container-custom-button">
-                  {" "}
-                  {/* Center content horizontally */}
-                  <button
-                    type="button"
-                    className="btn btn-primary btn-block custom-button"
-                    onClick={handleDebit}
-                    style={{
-                      width: "350px",
-                      borderRadius: "8px",
-                      transition: "transform 0.2s",
-                      marginTop: "2rem",
-                    }}
-                  >
-                    Debit Wallet
-                  </button>
-                </div>
-              </form>
+                  <div className="d-flex justify-content-center container-custom-button">
+                    {" "}
+                    {/* Center content horizontally */}
+                    <button
+                      type="button"
+                      className="btn btn-primary btn-block custom-button"
+                      onClick={handleDebit}
+                      style={{
+                        width: "350px",
+                        borderRadius: "8px",
+                        transition: "transform 0.2s",
+                        marginTop: "2rem",
+                      }}
+                    >
+                      Debit Wallet
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
-
-
       </div>
-
-
-
-
     </div>
   );
 };
