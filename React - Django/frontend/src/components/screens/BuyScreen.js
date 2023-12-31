b4769f4f31f59d678931ed24509905de60e23694
webapp-master <ogundirantoluwani@gmail.com>
2023-12-31 21:00:38 +0100

    I appply text-transform to the text inside the buttons inside the BuyScreen.js

--- React - Django/frontend/src/components/screens/BuyScreen.js
+++ React - Django/frontend/src/components/screens/BuyScreen.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { Col, Container, Row, Card, Button } from 'react-bootstrap';
+import React from "react";
+import { useSelector } from "react-redux";
+import { Col, Container, Row, Card, Button } from "react-bootstrap";
 
 const BuyScreen = () => {
   const cart = useSelector((state) => state.cart);
@@ -9,23 +9,23 @@ const BuyScreen = () => {
   // Assuming shippingAddress is available in the Redux state
   const shippingAddress = useSelector((state) => state.shippingAddress);
   const { shippingCost, phoneNumber } = shippingAddress;
-  
+
   const headerFooterColor = "#bdbdbd"; // Deeper version of the color
   const cardBodyColor = "#fff7eb"; // Desired background color for the card body
   const listBorderStyle = {
-    listStyleType: 'none',
-    padding: '10px',
-    border: '1px solid #000',
-    borderRadius: '25px',
-    backgroundColor: '#fff',
+    listStyleType: "none",
+    padding: "10px",
+    border: "1px solid #000",
+    borderRadius: "25px",
+    backgroundColor: "#fff",
   };
   const buttonStyle = {
-    width: '100%', // Setting a fixed width for all buttons
-    margin: '5px', // Adding some space between buttons
+    width: "100%", // Setting a fixed width for all buttons
+    margin: "5px", // Adding some space between buttons
   };
 
   const boldText = {
-    fontWeight: '900', // Making the text bold
+    fontWeight: "900", // Making the text bold
   };
 
   const renderProductImages = () => {
@@ -38,12 +38,15 @@ const BuyScreen = () => {
 
       imageRows.push(
         <Row key={i} className="mb-1">
-
           <Col xs={6} md={6}>
             <img
               src={item1.image}
               alt={item1.name}
-              style={{ maxWidth: '100%', marginBottom: '10px', borderRadius: '30px' }}
+              style={{
+                maxWidth: "100%",
+                marginBottom: "10px",
+                borderRadius: "30px",
+              }}
             />
           </Col>
 
@@ -52,13 +55,18 @@ const BuyScreen = () => {
               <img
                 src={item2.image}
                 alt={item2.name}
-                style={{ maxWidth: '100%', marginBottom: '10px', borderRadius: '30px' }}
+                style={{
+                  maxWidth: "100%",
+                  marginBottom: "10px",
+                  borderRadius: "30px",
+                }}
               />
             )}
-            {!item2 && isOdd && <div style={{ width: '100%', marginBottom: '10px' }} />}
+            {!item2 && isOdd && (
+              <div style={{ width: "100%", marginBottom: "10px" }} />
+            )}
             {/* Empty placeholder Col for alignment */}
           </Col>
-
         </Row>
       );
     }
@@ -70,10 +78,9 @@ const BuyScreen = () => {
     const totalItems = cartItems.reduce((accumulator, currentItem) => {
       return accumulator + currentItem.qty;
     }, 0);
-  
+
     return totalItems;
   };
-
 
   // Function to calculate the total cost of all items in the cart
   const calculateTotalCost = () => {
@@ -86,122 +93,147 @@ const BuyScreen = () => {
     return totalCost;
   };
 
-   // Calculate the total cost
-   const totalCost = calculateTotalCost();
+  // Calculate the total cost
+  const totalCost = calculateTotalCost();
 
-
-   const calculateTotalTax = () => {
+  const calculateTotalTax = () => {
     // Calculate the total tax from all cart items
     const totalTax = cartItems.reduce((accumulator, currentItem) => {
       // Parse the tax value to ensure it's treated as a number
       const taxValue = parseFloat(currentItem.tax);
       const quantity = currentItem.qty || 1; // Use a default quantity of 1 if not provided
-  
+
       const taxPerItem = isNaN(taxValue) ? 0 : taxValue * quantity; // Calculate tax per item
       return accumulator + taxPerItem;
     }, 0);
-  
+
     return totalTax.toFixed(2); // Limit the total tax to 2 decimal places
   };
-  
+
   // Calculate the total tax
   const totalTax = calculateTotalTax();
-
 
   const calculateTotalAmount = () => {
     // Calculate the total amount by summing up the cost of products, shipping cost, and tax
     const totalAmount = totalCost + shippingCost + parseFloat(totalTax);
     return totalAmount.toFixed(2); // Limit the total amount to 2 decimal places
   };
-  
+
   // Calculate the total amount
   const totalAmount = calculateTotalAmount();
-
 
   const formatShippingAddress = () => {
     // Access the shippingAddress from the Redux state
     const { houseNumber, street, area, city, state } = shippingAddress;
-  
+
     // Concatenate the address attributes in the desired order
     const concatenatedAddress = `${houseNumber}, ${street} Street, ${area}, ${city}, ${state}-State`;
     return concatenatedAddress;
   };
-  
 
   const capitalizeWords = (str) => {
     return str.replace(/\b\w/g, (char) => char.toUpperCase());
   };
-  
+
   // Get the concatenated shipping address and capitalize each word
   const concatenatedShippingAddress = formatShippingAddress();
-  const capitalizedShippingAddress = capitalizeWords(concatenatedShippingAddress);
-  
-  
-  
+  const capitalizedShippingAddress = capitalizeWords(
+    concatenatedShippingAddress
+  );
 
-
-
-
-
+  // Function to convert text to title case
+  const toTitleCase = (str) => {
+    return str.replace(/\w\S*/g, (txt) => {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+  };
 
   return (
     <Container>
       <Row>
-
         {/* Left Column: Product Images */}
-        <Col xs={12} md={6}> 
+        <Col xs={12} md={6}>
           {renderProductImages()}
         </Col>
 
-
         {/* Right Column: Order Details */}
         <Col xs={12} md={6}>
-          <Card style={{ border: 'none' }}> {/* Set border to none */} 
-
-
-            <Card.Header className="text-center" style={{ backgroundColor: headerFooterColor, borderRadius: "25px"}}>
+          <Card style={{ border: "none" }}>
+            {" "}
+            {/* Set border to none */}
+            <Card.Header
+              className="text-center"
+              style={{
+                backgroundColor: headerFooterColor,
+                borderRadius: "25px",
+              }}
+            >
               <h4>Place your Order</h4>
             </Card.Header>
-
-            <Card.Body className="text-dark" style={{ backgroundColor: cardBodyColor, borderRadius: "75px" }}>
-
+            <Card.Body
+              className="text-dark"
+              style={{ backgroundColor: cardBodyColor, borderRadius: "75px" }}
+            >
               <Card.Text className="text-center">
-                  <p style={boldText}>Wallet Balance: $700</p>
+                <p style={boldText}>Wallet Balance: $700</p>
 
-                  <p>Product List:</p>
-                  <ul style={listBorderStyle}>
-                    {cartItems.map((item, index) => (
-                      <li key={index}>{item.name}</li>
-                    ))}
-                  </ul>
+                <p>Product List:</p>
+                <ul style={listBorderStyle}>
+                  {cartItems.map((item, index) => (
+                    <li key={index}>{item.name}</li>
+                  ))}
+                </ul>
 
-                  <p>No. of Items: {calculateTotalItems()}</p>
+                <p>No. of Items: {calculateTotalItems()}</p>
 
-                  <p>Cost of Products in Your Cart: ${totalCost}</p>
+                <p>Cost of Products in Your Cart: ${totalCost}</p>
 
-                  <p>Shipping Cost: ${shippingCost}</p>
+                <p>Shipping Cost: ${shippingCost}</p>
 
-                  <p>Tax: ${totalTax}</p>
+                <p>Tax: ${totalTax}</p>
 
-                  <p style={boldText}>Total Amount: ${totalAmount}</p>
+                <p style={boldText}>Total Amount: ${totalAmount}</p>
 
-                  <p>Shipping Address: {capitalizedShippingAddress}</p>
+                <p>Shipping Address: {capitalizedShippingAddress}</p>
 
-                  <p>Phone Number: {phoneNumber}</p>
-
+                <p>Phone Number: {phoneNumber}</p>
               </Card.Text>
             </Card.Body>
 
-            <Card.Footer className="d-flex justify-content-between align-items-center" style={{ backgroundColor: headerFooterColor, borderRadius: "25px"}}>
-              <Button variant="secondary" className="rounded-pill" style={buttonStyle}>Cart</Button>
-              <Button variant="primary" className="rounded-pill" style={buttonStyle}>Purchase</Button>
-              <Button variant="success" className="rounded-pill" style={buttonStyle}>Shipping</Button>
+
+            <Card.Footer
+              className="d-flex justify-content-between align-items-center"
+              style={{
+                backgroundColor: headerFooterColor,
+                borderRadius: "25px",
+              }}
+            >
+              <Button
+                variant="danger"
+                className="rounded-pill titleCaseText"
+                style={buttonStyle}
+              >
+                Make Payment
+              </Button>
+              <Button
+                variant="success"
+                className="rounded-pill titleCaseText"
+                style={buttonStyle}
+              >
+                Fund Wallet
+              </Button>
+              <Button
+                variant="primary"
+                className="rounded-pill titleCaseText"
+                style={buttonStyle}
+              >
+                WhatsApp us
+              </Button>
             </Card.Footer>
 
+            
           </Card>
         </Col>
-
-
       </Row>
     </Container>
   );
