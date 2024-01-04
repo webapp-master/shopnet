39583633a7265c754814b94479c6beedbbe85d5a
webapp-master <ogundirantoluwani@gmail.com>
2024-01-04 20:36:33 +0100

    I a back

--- React - Django/frontend/src/components/screens/BuyScreen.js
+++ React - Django/frontend/src/components/screens/BuyScreen.js
@@ -141,12 +141,7 @@ const BuyScreen = () => {
     concatenatedShippingAddress
   );
 
-  // Function to convert text to title case
-  const toTitleCase = (str) => {
-    return str.replace(/\w\S*/g, (txt) => {
-      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-    });
-  };
+  
 
   return (
     <Container>
@@ -231,7 +226,7 @@ const BuyScreen = () => {
               </Button>
             </Card.Footer>
 
-            
+
           </Card>
         </Col>
       </Row>
