9851d6fd8ce7fdbcba46095c56832f2b09783d20
webapp-master <ogundirantoluwani@gmail.com>
2024-01-25 21:08:01 +0100

    I  have a framework  for the TransactionDetailsScreen.js.... needs further development

--- React - Django/frontend/src/components/screens/TransactionDetailsScreen.js
+++ React - Django/frontend/src/components/screens/TransactionDetailsScreen.js
@@ -1,17 +1,125 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import {
+  Container,
+  Row,
+  Col,
+  Table,
+  InputGroup,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const TransactionDetailsScreen = ({ match }) => {
   const transactionId = match.params.id;
+
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  // Access the token from Redux state
+  const accessToken = useSelector((state) => state.userLogin.userInfo?.access);
+  console.log("Access Token:", accessToken);
+
+  // Get user information from Redux state
+  const userInfo = useSelector((state) => state.userLogin.userInfo);
 
   // You can use the transactionId to fetch details of the specific transaction
   // and render the details on this page.
 
   return (
-    <div>
-      <h2>Transaction Details</h2>
-      <p>Transaction ID: {transactionId}</p>
-      {/* Add more details as needed */}
-    </div>
+    <Container fluid>
+      <Row className="justify-content-center">
+        <Col md={12}>
+          <div className="wallet-container">
+            <div className="wallet-header">
+              <div className="wallet-balance">
+                <p style={{}}></p>
+              </div>
+
+              <h1>Transaction Details: {transactionId}</h1>
+
+              <div className="search-bar">
+                <InputGroup className="search-field"></InputGroup>
+              </div>
+            </div>
+
+           
+              
+              <Table responsive className="transaction-table">
+                <thead>
+                  <tr>
+                    <th>Reference</th>
+                    <th>Amount</th>
+                    <th>Description</th>
+                    <th>Previous Balance</th>
+                    <th>New Balance</th>
+                    <th>Date/Time</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  <tr>
+                    <td>23344</td>
+                    <td>456</td>
+                    <td>hhdkdddddddddddd</td>
+                    <td>444444</td>
+                    <td>443333</td>
+                    <td>3333333</td>
+                  </tr>
+
+                  <tr>
+                    <td>23344</td>
+                    <td>456</td>
+                    <td>hhdkdddddddddddd</td>
+                    <td>444444</td>
+                    <td>443333</td>
+                    <td>3333333</td>
+                  </tr>
+
+                  <tr>
+                    <td>23344</td>
+                    <td>456</td>
+                    <td>hhdkdddddddddddd</td>
+                    <td>444444</td>
+                    <td>443333</td>
+                    <td>3333333</td>
+                  </tr>
+
+                  <tr>
+                    <td>23344</td>
+                    <td>456</td>
+                    <td>hhdkdddddddddddd</td>
+                    <td>444444</td>
+                    <td>443333</td>
+                    <td>3333333</td>
+                  </tr>
+
+                  <tr>
+                    <td>23344</td>
+                    <td>456</td>
+                    <td>hhdkdddddddddddd</td>
+                    <td>444444</td>
+                    <td>443333</td>
+                    <td>3333333</td>
+                  </tr>
+
+                  <tr>
+                    <td>23344</td>
+                    <td>456</td>
+                    <td>hhdkdddddddddddd</td>
+                    <td>444444</td>
+                    <td>443333</td>
+                    <td>3333333</td>
+                  </tr>
+                </tbody>
+              </Table>
+            
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
