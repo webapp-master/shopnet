613ab388a5e15fbd9c98ecca4ae4af1263a858cd
webapp-master <ogundirantoluwani@gmail.com>
2024-01-30 20:17:05 +0100

    developing the empty Col

--- React - Django/frontend/src/components/screens/ProductScreen.js
+++ React - Django/frontend/src/components/screens/ProductScreen.js
@@ -33,50 +33,80 @@ function ProductScreen({ match, history }) {
   return (
     <Container fluid>
       <div className="product">
-
         <Link to="/">
           <Button className="btn-block product-custom my-1" type="button">
             Go Back
           </Button>
         </Link>
 
-
         {loading ? (
           <Loader />
         ) : error ? (
           <Message variant="danger">{error} </Message>
         ) : (
-
-      
-
           <Row>
             <Col md={6}>
-              <Image style={{borderRadius: "10px"}} src={product.image} alt={product.name} fluid />
-              
+              <Image
+                style={{ borderRadius: "10px" }}
+                src={product.image}
+                alt={product.name}
+                fluid
+              />
             </Col>
 
-            
-          </Row>
+            <Col md={6}>
 
+
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                }}
+              >
+                {/* Container 1 */}
+                <div
+                  style={{
+                    marginBottom: "20px",
+                    padding: "10px",
+                    border: "1px solid #ccc",
+                    borderRadius: "8px",
+                    backgroundColor: "red"
+                  }}
+                >
+                  {/* Content for Container 1 */}
+                  {/* You can add your content here */}
+                </div>
+
+                {/* Container 2 */}
+                <div
+                  style={{
+                    padding: "10px",
+                    border: "1px solid #ccc",
+                    borderRadius: "8px",
+                    backgroundColor: "blue"
+                  }}
+                >
+                  {/* Content for Container 2 */}
+                  {/* You can add your content here */}
+                </div>
+              </div>
+
+
+            </Col>
+          </Row>
         )}
 
-
-          <Row>
-            <Col md={6}>
-              <ListGroup.Item className="">
-                <div>
-                  <p style={{ fontSize: "20px", color: "red" }}> Description: </p>
-                  {product.description}
-                </div>
-              </ListGroup.Item>
-            </Col>
-          </Row>
-
-
-
-         
-
-        
+        <Row>
+          <Col md={6}>
+            <ListGroup.Item className="">
+              <div>
+                <p style={{ fontSize: "20px", color: "red" }}> Description: </p>
+                {product.description}
+              </div>
+            </ListGroup.Item>
+          </Col>
+        </Row>
       </div>
     </Container>
   );
