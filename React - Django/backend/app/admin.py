7ced7b599d4cade065631f71f7e4964888586e75
webapp-master <ogundirantoluwani@gmail.com>
2024-02-19 15:26:35 +0100

    set up admin interface on django

--- React - Django/backend/app/admin.py
+++ React - Django/backend/app/admin.py
@@ -4,19 +4,22 @@ from app.models import *
 
 class OrderItemAdmin(admin.ModelAdmin):
     readonly_fields = ('product', 'order', 'qty', 'price', 'image', 'unitTax', 'status_created_at',)
-    list_display = ('product', 'order', 'qty', 'price', 'image', 'unitTax', 'status', 'status_created_at',)
+    list_display = ('product','qty', 'price', 'status', 'status_created_at',)
     list_editable = ('status',)  # Makes the 'status' field editable in the list view
 
     # Optionally, you can customize the form fields for the add and change views
-    # fields = ('status',)  # If you want to limit the fields shown in the add and change views
+    #fields = ('status',)  # If you want to limit the fields shown in the add and change views
 
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'details', 'order', 'type', 'timestamp')
+    
 
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Product)
 admin.site.register(Wallet)
-admin.site.register(Transaction)
+admin.site.register(Transaction, TransactionAdmin)
 admin.site.register(Order)
 admin.site.register(ShippingAddress)
 admin.site.register(Supplier)
