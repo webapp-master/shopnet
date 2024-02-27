4096c484e3724fa216e032a6df92ea7ef33088cd
webapp-master <ogundirantoluwani@gmail.com>
2024-02-27 18:11:14 +0100

    I added logic to display 00:00:00:00 when the value coming from the backend is negative

--- React - Django/frontend/src/components/screens/TransactionDetailsScreen.js
+++ React - Django/frontend/src/components/screens/TransactionDetailsScreen.js
@@ -38,6 +38,10 @@ const TransactionDetailsScreen = ({ match }) => {
 
   // Function to convert milliseconds to readable time format
   const convertMillisecondsToTime = (milliseconds) => {
+    // If milliseconds is negative, return '00:00:00:00'
+    if (milliseconds < 0) {
+      return "00:00:00:00";
+    }
     const seconds = Math.floor((milliseconds / 1000) % 60);
     const minutes = Math.floor((milliseconds / (1000 * 60)) % 60);
     const hours = Math.floor((milliseconds / (1000 * 60 * 60)) % 24);
