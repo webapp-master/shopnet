4adcdff6e59717e430b29d9e2bc2bb5533857f65
webapp-master <ogundirantoluwani@gmail.com>
2024-05-08 20:37:39 +0100

    I modified the urls.py/backend  so that i the root url can browse the  the /admin

--- React - Django/backend/backend/urls.py
+++ React - Django/backend/backend/urls.py
@@ -3,11 +3,15 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponseRedirect
 
 
 urlpatterns = [
+    path('', lambda request: HttpResponseRedirect('/admin/')),  # Redirect root URL to '/admin'
     path('admin/', admin.site.urls),
-    path('api/',include('app.urls')),
+    path('api/', include('app.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+# Optional: Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
